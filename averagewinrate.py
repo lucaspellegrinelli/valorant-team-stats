8158cd4c701842fb420eb9453a447096f289e2e1
Lucas Pellegrinelli <lucasresende@dcc.ufmg.br>
2022-05-08 19:06:42 -0300

    first commit

---
+++ averagewinrate.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/player-data.csv")
+df = df[df["episode"] == 3]
+
+df["season"] = 3 * df["episode"] + df["act"]
+df["matches"] = df["wins"] + df["losses"]
+
+maps = set(df["mapname"])
+
+gb = df.groupby(["mapname", "player"]).mean().reset_index()
+winrates = []
+for mapname in maps:
+    winrates.append((mapname, np.mean(gb[gb["mapname"] == mapname]["winrate"])))
+
+winrates.sort(key=lambda wr: wr[1], reverse=True)
+for mapname, wr in winrates:
+    print(mapname, f"{round(wr * 100, 2)}%")
---
+++ mapbinomial.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+plt.style.use("seaborn-dark")
+
+df = pd.read_csv("data/player-data.csv")
+df["season"] = 3 * df["episode"] + df["act"]
+df["matches"] = df["wins"] + df["losses"]
+
+gb = df.groupby(["mapname"]).sum().reset_index()
+
+figure = plt.gcf()
+
+x = np.linspace(0, 1, 1000)
+for index, row in gb.iterrows():
+    r = row["wins"]
+    n = row["wins"] + row["losses"]
+    wr = round(r / n * 100, 1)
+    distribution = stats.binom.pmf(r, n, x)
+    plt.plot(x, distribution, label=f"{row['mapname']} ({wr}% / {n})")
+
+plt.ylabel("P(P(X))")
+plt.xlabel("P(X) where X = us winning the map")
+plt.legend()
+figure.set_size_inches(8, 4)
+plt.tight_layout()
+plt.show()
+# plt.savefig("plots/map-binomial.png")
